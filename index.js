809e078e323b4783bf4d8ad3bb6c1744ec5b2e31
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-12-08 08:03:08 -0300

    Generating the URL

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ document.querySelector('form').addEventListener('submit', event => {
   const input = document.querySelector('#message-input');
   const encrypted = btoa(input.value);
 
-  document.querySelector('#link-input').value = encrypted;
+  const linkInput = document.querySelector('#link-input')
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
 });
