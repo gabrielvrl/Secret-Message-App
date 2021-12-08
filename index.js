08f6b6c5c81a0179c34b8f73726aead409c3d4f8
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-12-08 07:42:11 -0300

    handling form submission

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  const input = document.querySelector('input');
+
+  console.log(input.value)
+});
